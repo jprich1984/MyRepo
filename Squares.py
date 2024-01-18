bb924d8517583a20e922a02878384ae8f4b12e13
BCS Staff <bcsstaff@BCSs-MacBook-Air.local>
2024-01-18 14:33:57 -0800

    Lambda Function

--- Squares.py
+++ Squares.py
@@ -1,10 +1,8 @@
 
 import sys
 
-def Squares(n):
-	L=[]
-	for i in range(n):
-		L.append(i**2)
-	return L
+def Squares(n,fn=lambda x: x**2):
+	
+	return [fn(i) for i in range(n)]
 
 print(Squares(int(sys.argv[1])))		
