e7d319bc234a2252576b50b7679c87a0d842227f
BCS Staff <bcsstaff@BCSs-MacBook-Air.local>
2024-01-18 22:39:20 -0800

    done with scaled sigmoid

--- Squares.py
+++ Squares.py
@@ -1,5 +1,6 @@
 
 import sys
+import numpy as np
 """Calculates the perfect squares up to a provided number"""
 
 def squares(n,fn=lambda x: x**2):
@@ -8,4 +9,10 @@ def squares(n,fn=lambda x: x**2):
 
 def square(n):
 	return n**2
-print(squares(int(sys.argv[1])))		
+print(f'Squares from 0 to {sys.argv[1]}:{squares(int(sys.argv[1]))}')
+
+def sigmoid(x,a=1):
+	return 1/(1+np.exp(-a*x))
+
+print(f'Scaled Sigmoid of {sys.argv[1]}: {sigmoid(int(sys.argv[1]))}')	
+			
