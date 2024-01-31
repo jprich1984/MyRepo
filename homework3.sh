b8cdea870c461dad1a5ebd9c898961285c2d3e39
jake Prichard <jprich1984@gmail.com>
2024-01-31 02:48:02 -0800

    commit new file

---
+++ homework3.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Bash script to calculates the MAX, MIN, MEDIAN and MEAN of the word frequencies in the
+# file the  https://www.gutenberg.org/files/58785/58785-0.txt
+
+if [ $# -ne 1 ]
+   then
+       echo "Please provide a txt file url"
+       echo "usage ./calculate_basic_stats.sh  url"
+       #exit with error
+       exit 1
+file=$1   
+
+echo  "############### Statistics for file  ############### "
+
+# Q1(.5 point) write  positional parameter after echo to print its value. It is the file url used by curl command.
+
+echo "$1"
+
+
+# sort based on multiple columns
+#Q2(2= 1+1 for right sorting of each columns). Write last sort command options so that first column(frequencies) is
+#sorted via numerical values and
+#second column is sorted by reverse alphabetical order
+
+sorted_words=`curl -s $1|tr [A-Z] [a-z]|grep -oE "\w+"|sort|uniq -c|sort -nr  `
+
+
+total_uniq_words=`echo "$sorted_words"|wc -l`
